c03d2c3e0e7413831d15adb5847dd521a193578a
MatthieuLzn <Matthieu.l94@hotmail.fr>
2021-07-05 11:37:54 +0200

    5 premier exos

---
+++ exo_05.rb
@@ -0,0 +1,23 @@
+puts "On va compter le nombre d'heures de travail à THP" #Affiche "On va compter ..."
+
+puts "Travail : #{10 * 5 * 11}" # L'opération #{x + b * c} permet d'afficher le résultat directement
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" # Pareil
+
+puts "Et en seconde ?" # Affiche "Et en seconde ? "
+
+puts 10 * 5 * 11 * 60 * 60  # Affiche uniquement le resultat
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" #affiche le texte
+
+puts 3 + 2 < 5 - 7 # Vue qu'il y à un opérateur booléens ( Le < ) cela rapporte uniquement false ou true
+
+puts "ça fait combien 3 + 2 ? #{3 + 2}" # Rapporte le resultat
+puts "ça fait combien 5 - 7 ? #{5 - 7}" # PAreil
+
+puts "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en-plus :"
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" #Rapporte false ou true
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"#Pareil
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"#Pareil
